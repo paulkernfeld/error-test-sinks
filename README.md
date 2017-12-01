0e2bd2024140ea29a35d43cf8e4526708becb210
Paul Kernfeld <paulkernfeld@gmail.com>
2017-12-01 17:48:32 -0500

    Better top-level docs

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 `Sink` implementations for testing that simulate different types of errors.
 
+### Sinks:
+- `StartSendErrSink`: returns an error on `start_send`
+- `PollCompleteErrSink`: returns an error on `poll_complete`
+- `CloseErrSink`: returns an error on `close`
+
 License: MIT/Apache-2.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 #![no_std]
 #![deny(warnings)]
 //! `Sink` implementations for testing that simulate different types of errors.
+//!
+//! ## Sinks:
+//! - `StartSendErrSink`: returns an error on `start_send`
+//! - `PollCompleteErrSink`: returns an error on `poll_complete`
+//! - `CloseErrSink`: returns an error on `close`
 extern crate futures;
 
 pub use start_send_error_sink::StartSendErrSink;
